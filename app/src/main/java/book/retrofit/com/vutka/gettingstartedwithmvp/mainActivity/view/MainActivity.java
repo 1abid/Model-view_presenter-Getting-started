4da9d91ef22f3308718a3c14ff364058ac36851a
Sharmin Sultana <la4508@gmail.com>
2016-11-29 15:50:30 +0600

    initial commit for this repo

---
+++ app/src/main/java/book/retrofit/com/vutka/gettingstartedwithmvp/mainActivity/view/MainActivity.java
@@ -0,0 +1,15 @@
+package book.retrofit.com.vutka.gettingstartedwithmvp.mainActivity.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import book.retrofit.com.vutka.gettingstartedwithmvp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
